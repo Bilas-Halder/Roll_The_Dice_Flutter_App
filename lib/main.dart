0e133353fd1d43a4786c49939ef4d476aaaf72b2
Bilas-Halder <bilas8642@gmail.com>
2022-01-22 02:11:41 +0600

    2 dice button added with onpress functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -19,15 +20,59 @@ void main() {
   ));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+  void changeDiceNum(int side) {
+    setState(() {
+      if (side == 1) {
+        leftDiceNum = Random().nextInt(6) + 1;
+      } else if (side == 2) {
+        rightDiceNum = Random().nextInt(6) + 1;
+      } else {
+        leftDiceNum = Random().nextInt(6) + 1;
+        rightDiceNum = Random().nextInt(6) + 1;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Expanded(
-          child: Image(
-            image: AssetImage('images/dice1.png'),
-          ),
+        Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: TextButton(
+                  child: Image.asset('images/dice$leftDiceNum.png'),
+                  onPressed: () {
+                    changeDiceNum(1);
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: TextButton(
+                  child: Image.asset('images/dice$rightDiceNum.png'),
+                  onPressed: () {
+                    changeDiceNum(2);
+                  },
+                ),
+              ),
+            ),
+          ],
         )
       ],
     );
